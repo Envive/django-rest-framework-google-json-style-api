3881fd80a323ff02c0534027a77336a3f16352e3
pc-huang <pocheng@outlook.com>
2019-01-16 14:44:48 +0800

    Add pagination

--- rest_framework_google_json_style_api/pagination.py
+++ rest_framework_google_json_style_api/pagination.py
@@ -1,5 +1,26 @@
+from collections import OrderedDict
+
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.views import Response
 
 
 class GoogleJsonStylePageNumberPagination(PageNumberPagination):
-    pass
+    page_query_param = 'page'
+    page_size_query_param = 'page_size'
+    page_size = 10
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+
+        return Response(
+            OrderedDict([
+                ('current_item_count', len(data)),
+                ('itemsPerPage', self.page_size),
+                ('total_items', self.page.paginator.count),
+                ('page_index', self.page.number),
+                ('total_pages', self.page.paginator.num_pages),
+                ('next_link', self.get_next_link()),
+                ('previous_link', self.get_previous_link()),
+                ('results', data),
+            ])
+        )
--- rest_framework_google_json_style_api/renderers.py
+++ rest_framework_google_json_style_api/renderers.py
@@ -13,12 +13,23 @@ class JSONRenderer(api_settings.RENDERER_CLASS):
         google_style_params.update(
             renderer_context.get('request').query_params.items()
         )
+
+        if data and 'results' in data:
+            # Pagination
+            data['items'] = data['results']
+            data.pop('results', None)
+            serialize_data = data
+        else:
+            serialize_data = {
+                'items': data if type(data) == list else [data]
+            }
+
         render_data = OrderedDict()
         render_data['method'] = renderer_context.get('view').action
         render_data['params'] = google_style_params
-        render_data['data'] = {
-            'items': data if type(data) == list else [data]
-        }
+        render_data['data'] = serialize_data
+
         if api_settings.CAMELIZE:
             render_data = camelize(render_data)
+
         return super(JSONRenderer, self).render(render_data, *args, **kwargs)
