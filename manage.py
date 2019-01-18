248a16eb6d47c46246f75025d984aab460da28ef
pc-huang <pocheng@outlook.com>
2019-01-18 16:59:55 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         raise
 
     # This allows easy placement of apps within the interior
-    # maltose directory.
+    # example directory.
     current_path = os.path.dirname(os.path.abspath(__file__))
     sys.path.append(os.path.join(current_path, "example"))
 
