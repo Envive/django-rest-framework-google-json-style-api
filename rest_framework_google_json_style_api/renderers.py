f63f115432d6c203bb07052a83cb959259de9321
Chieh Teng Chang <scott820914@hotmail.com>
2019-01-23 17:19:05 +0800

    Render error response (#10)
    * Render error response
    
    * Fix bugs
    
    * Revise import path

--- rest_framework_google_json_style_api/renderers.py
+++ rest_framework_google_json_style_api/renderers.py
@@ -4,11 +4,17 @@ from django_rest_framework_camel_case.util import camelize
 
 from rest_framework_google_json_style_api.settings import api_settings
 
+from rest_framework_google_json_style_api import utils
+
 
 class JSONRenderer(api_settings.RENDERER_CLASS):
-    def render(self, data, *args, **kwargs):
-        media_type, renderer_context = args
+    def render(self, data, accepted_media_type=None, renderer_context=None):
         renderer_context = renderer_context or {}
+
+        # If this is an error response, skip the rest.
+        if utils.is_error_response(renderer_context):
+            return self.render_errors(data, accepted_media_type, renderer_context)
+
         google_style_params = renderer_context.get('kwargs')
         google_style_params.update(
             renderer_context.get('request').query_params.items()
@@ -34,5 +40,10 @@ class JSONRenderer(api_settings.RENDERER_CLASS):
 
         if api_settings.CAMELIZE:
             render_data = camelize(render_data)
+        return super(JSONRenderer, self).render(render_data, accepted_media_type, renderer_context)
 
-        return super(JSONRenderer, self).render(render_data, *args, **kwargs)
+    def render_errors(self, data, accepted_media_type=None, renderer_context=None):
+        render_data = utils.format_errors(data, accepted_media_type, renderer_context)
+        if api_settings.CAMELIZE:
+            render_data = camelize(render_data)
+        return super(JSONRenderer, self).render(render_data, accepted_media_type, renderer_context)
